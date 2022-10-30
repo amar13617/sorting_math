f3a665b6d2d3e01c60619f51b306ca1072704b75
amar13617 <amar13617@gmail.com>
2022-10-30 12:21:05 +0530

    second commit

--- amplify/backend/function/sortlist/src/index.py
+++ amplify/backend/function/sortlist/src/index.py
@@ -11,5 +11,6 @@ def handler(event, context):
           'Access-Control-Allow-Origin': '*',
           'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
       },
-      'body': json.dumps('Hello from your new Amplify Python lambda!')
+      'body': json.dumps('Hello from Amar')
+
   }
