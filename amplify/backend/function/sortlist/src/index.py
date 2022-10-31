4e21896ba7cf91caad8bf4ae085dee519929a15c
amar13617 <amar13617@gmail.com>
2022-10-31 21:57:57 +0530

    even_odd commit

--- amplify/backend/function/sortlist/src/index.py
+++ amplify/backend/function/sortlist/src/index.py
@@ -3,14 +3,24 @@ import json
 def handler(event, context):
   print('received event:')
   print(event)
-  
-  return {
-      'statusCode': 200,
-      'headers': {
-          'Access-Control-Allow-Headers': '*',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-      },
-      'body': json.dumps('Hello from Amar singh from maner')
+  list1 = event.get("data")
+  operation = event.get("operation")
+  if operation == "find_odd_even":
+    return find_odd_even(list1)
+  else:
+    return { 
+   }
+def find_odd_even(data):
+    even_list = []
+    odd_list = []
+    for i in data:
+        if i % 2 == 0:
+            even_list.append(i)
+        else:
+            odd_list.append(i)
 
-  }
+    return {
+        "even_list" : even_list
+        "odd_list" : odd_list
+    }
+
