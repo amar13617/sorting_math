6d64c88e80c03d5f4214ccd7f888e890e16e6556
amar13617 <amar13617@gmail.com>
2022-12-21 19:34:20 +0530

    Third Commit

--- amplify/backend/function/sortlist/src/index.py
+++ amplify/backend/function/sortlist/src/index.py
@@ -6,47 +6,55 @@ dynamodb = boto3.resource('dynamodb')
 def handler(event, context):
   print('received event:')
   print(event)
-  bucket = 'surveysort'
-  survey = event['filename']
-  csv_object = s3_client.get_object(Bucket=bucket,Key=survey)
+  bucket = 'emp-detail'
+  Employee_detail = event['filename']
+  csv_object = s3_client.get_object(Bucket=bucket,Key=Employee_detail)
   print(csv_object)
   file_reader = csv_object['Body'].read().decode("utf-8")
   print(file_reader)
   user = file_reader.split("\n")
   print(user)
 
-#  test_sum = 0
-#  for index in range(1,len(user)):
-#    if not (user[index]):
-#      continue
-#    test_sum =test_sum + int(user[index].split(",")[2])
-  
-#  average_list = test_sum/(len(user)-1)
-#  print(average_list)
-  
-  
-
-  
-
-  
-
   list1 = []
   for index in range(1, len(user)):
     if not user[index]:
       continue
     list1.append(int(user[index].split(",")[2]))
+
+  list2 = []
+  for index in range(1, len(user)):
+    if not user[index]:
+      continue
+    list2.append(int(user[index].split(",")[4]))
+
+  list3 = []
+  for index in range(1, len(user)):
+    if not user[index]:
+      continue
+    list3.append(int(user[index].split(",")[1]))
   
-  filename = event.get("survey.csv")
+  list4 = []
+  for index in range(1, len(user)):
+    if not user[index]:
+      continue
+    list4.append(int(user[index].split(",")[5]))
+
+
+
+
+  filename = event.get("Employee_detail.csv")
   operation = event.get("operation")
 
-  if operation == "sort_list":
-    return find_sort(list1)
-  elif operation == "average":
+  if operation == "average":
     return find_average(list1)
-  elif operation == "reverse_list":
-    return reverse_number(list1)
-  elif operation == "odd_even":
-    return find_odd_even(list1)
+  elif operation == "sort":
+    return find_sort(list2)
+  elif operation == "greater_than > 5000":
+    return find_greater(list3)
+  elif operation == "remove element":
+    return remove_end(list4)
+  elif operation == "maximum value":
+    return maximum(list4)
   else:
     return {
 
@@ -55,36 +63,41 @@ def handler(event, context):
 def find_average(list1):
   average = sum(list1)/(len(list1))
   return {
-    "average_list" : average
+    "average_of_list" : average
   }
 
-
-def find_odd_even(list1):
-  even_list = []
-  odd_list = []
-  for index in list1:
-    if index % 2 == 0:
-      even_list.append(index)
-    else:
-      odd_list.append(index)
-
-  return {
-    "even" : even_list,
-    "odds" : odd_list
-  }
-
-def reverse_number(list1):
-  reverse_list = []
-  for index in reversed(list1):
-    reverse_list.append(index)
-  return {
-    "list_reverse" : reverse_list
-  }
-
-def find_sort(list1):
+def find_sort(list2):
   sort_list = []
-  for index in sorted(list1):
+  for index in sorted(list2):
     sort_list.append(index)
   return {
-    "sorting_list" : sort_list
+    "sorting" : sort_list
   }
+
+def find_greater(list3):
+  maximum = []
+  for index in (list3):
+    if index > 5000:
+      maximum.append(index)
+  return {
+    "max >5000" : maximum
+  }
+      
+def remove_end(list4):
+  list5 = []
+  for index in list4:
+    list5 =  list4[1:-1]
+    list5.append(index)
+  return {
+    "remove" :list5
+  }
+
+      
+  def maximum(list4):
+    maxi = list[0]
+    for i in list4:
+      if i > maxi:
+        maxi = i
+    return {
+      "maximum" : maxi
+    }
